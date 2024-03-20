7d54a069f30c696084f7f7c06dbdc4a6b4904374
Rizki Adi Wicaksono <rizkiadiw05@gmail.com>
2024-03-20 12:27:58 +0700

    contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama Rizki Adi Wicaksono
+ Saya tinggal di bekasi
+ olah raga yang saya sukai adalah
+ Bola Basket";
+ }
+}
