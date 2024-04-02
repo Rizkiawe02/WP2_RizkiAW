b75e1c27d1cd792100b29ce5317be186bae14a32
antoniayuliana <deruyuliana27@gmail.com>
2024-04-02 17:07:45 +0700

    WP Pertemuan 3

---
+++ views/v_about.php
@@ -0,0 +1,24 @@
+<head>
+<link rel="stylesheet" type="text/css" href="assets/css/stylebuku.css">
+</head>
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Rizki</li>
+ <li>Nama Belakang : Adi Wicaksono</li>
+ </ul>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> jalan btp</li>
+ </ul>
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Bekasi</li>
+ </ul>
+ <h4>OlahRaga dan Hobby Favorit</h4>
+ <ul type="square">
+ <li>Bola basket</li>
+ <li>Vapers</li>
+ </ul>
+</section
